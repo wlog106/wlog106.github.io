47ec07b9f6744dc7d1f07d63dca9b09c9e496e1b
leowu82 <wuzonghua82@gmail.com>
2024-10-30 16:46:29 +0800

    Template done

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basic interactivity for the site
+document.addEventListener("DOMContentLoaded", () => {
+    // You can add interactive features here
+    // Example: alert when clicking a button
+    document.getElementById("about").addEventListener("click", () => {
+        alert("About section clicked!");
+    });
+});
